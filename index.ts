d96dd62c790c0bdf06b99f44c5acf08440f1f5df
joseph omorodion <josephomorodion2018@gmail.com>
2023-07-11 12:22:52 -0700

    ninthc

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-export const handler = (event)=>{
+export const handler = (event)=> {
     if (event.httpMethod === "GET" && event.path === "/first") {
         const response:any = {statusCode:200,
             body:"hello world"}
             return response
         
     }
+
 }
